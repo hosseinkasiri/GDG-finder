dde0f5cf546583d3631bffdd740a4e86bcf5d117
hosseinkasiri <hosseinkasiri5@gmail.com>
2021-03-25 02:01:52 +0430

    add darkMode

--- app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
+++ app/src/main/java/com/example/android/gdgfinder/home/HomeFragment.kt
@@ -5,8 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
 import com.example.android.gdgfinder.R
+import com.example.android.gdgfinder.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
 
@@ -17,12 +21,20 @@ class HomeFragment : Fragment() {
     private lateinit var viewModel: HomeViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.home_fragment, container, false)
+        val binding = HomeFragmentBinding.inflate(inflater)
         viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-
-        return view
+        binding.viewModel = viewModel
+        viewModel.navigateToSearch.observe(viewLifecycleOwner,
+                Observer<Boolean> { shouldNavigate ->
+                    if (shouldNavigate == true) {
+                        val navController = binding.root.findNavController()
+                        navController.navigate(R.id.action_homeFragment_to_gdgListFragment)
+                        viewModel.onNavigatedToSearch()
+                    }
+                })
+        return binding.root
     }
 }
